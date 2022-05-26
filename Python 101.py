647b4bf17c71aa7728106d06f0899fa5d037ff4f
sayampalrecha <sayampalrecha09@gmail.com>
2022-05-26 23:32:20 +0530

    101

---
+++ Python 101.py
@@ -0,0 +1,9 @@
+# Using regular def function
+def square(x):
+    return x*x
+square(5)
+
+# Using lambda function
+lambda_square = lambda x : x*x
+lambda_square(5)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-OTc4OTY2MDA1MjM5OTE4NjMy.G_f3_U.pbikmZX6GLW--Gfy5Va_geO4CTTq6XZad3HAfI
+square(5)
