e059c15bea7aa4f8f88f6b8f87792307c674907c
sayampalrecha <sayampalrecha09@gmail.com>
2022-05-27 09:58:51 +0530

    101

--- Python 101.py
+++ Python 101.py
@@ -1,9 +1,3 @@
-# Using regular def function
-def square(x):
-    return x*x
-square(5)
-
-# Using lambda function
-lambda_square = lambda x : x*x
-lambda_square(5)
+# Creating a nested list 
+L = ['a', ['bb', ['ccc', 'ddd'], 'ee', 'ff'], 'g', 'h']
 
