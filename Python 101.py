2515b535b8b69ce226574a824a6056b49be1b8c1
sayampalrecha <sayampalrecha09@gmail.com>
2022-06-06 00:09:12 +0530

    101

--- Python 101.py
+++ Python 101.py
@@ -1,9 +1,15 @@
+# OOPS in Python 
+
+# define a class called Car() 
 class Car:
     def __init__(self,color,price):
         self.color = color
         self.price = price
 
+# object_name is car 
 car = Car('red',45000)
 
 print(car.color)
 print(car.price)
+
+
