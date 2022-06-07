141f87a2a889fd818a23120638a2146879072ff2
sayampalrecha <sayampalrecha09@gmail.com>
2022-06-07 21:54:02 +0530

    OOP

--- Python 101.py
+++ Python 101.py
@@ -1,2 +1,31 @@
-# pandas
+# OOP IN PYTHON
 
+# POLYMORPHISM IN PYTHON 
+
+
+class Dog():
+    
+    def __init__(self,name):
+        self.name = name
+    
+    def speak(self):
+        return self.name + " says woof!"
+
+class Cat():
+    
+    def __init__(self,name):
+        self.name = name
+    
+    def speak(self):
+        return self.name + " says meow"
+    
+dog = Dog("MAX")
+cat = Cat("MILO")
+
+print(dog.name)
+print(dog.speak())
+
+print(cat.name)
+print(cat.speak())
+          
+    
