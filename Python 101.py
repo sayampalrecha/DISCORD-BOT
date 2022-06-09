565503454684e423cb578f01161886b653d4e093
sayampalrecha <sayampalrecha09@gmail.com>
2022-06-10 01:35:19 +0530

    pandas

--- Python 101.py
+++ Python 101.py
@@ -1,2 +1,13 @@
-# MADE FOR DAY 39
-# made for day 40 :)
+# how to create a dataframe using numpy and pandas 
+
+import pandas as pd
+import numpy as np
+
+mydata = [[45,55,65],[44,55,66],[77,22,33]]
+# print(np.array(mydata))    printts the np array
+myindex = ['USA','INDIA','CHINA']
+mydata1 = ['POP','GDP','AREA']
+
+df = pd.DataFrame(mydata,myindex,mydata1)
+print(df)
+
